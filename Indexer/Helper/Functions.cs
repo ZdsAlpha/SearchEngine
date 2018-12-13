45cced799e63adf6bf93d7b7328e33566ee9ae3c
ZdsAlpha <abzsoft999@gmail.com>
2018-12-14 02:49:57 +0500

    Added base

---
+++ Indexer/Helper/Functions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Helper
+{
+    public static class Functions
+    {
+        static readonly string[] Suffixes =
+                   {"", "K", "M", "G", "T", "P", "E", "Z", "Y" };
+        static string Suffix(long value, int decimalPlaces = 2)
+        {
+            if (decimalPlaces < 0) { throw new ArgumentOutOfRangeException("decimalPlaces"); }
+            if (value < 0) { return "-" + Suffix(-value); }
+            if (value == 0) { return string.Format("{0:n" + decimalPlaces + "} bytes", 0); }
+            int mag = (int)Math.Log(value, 1024);
+            decimal adjustedSize = (decimal)value / (1L << (mag * 10));
+            if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
+            {
+                mag += 1;
+                adjustedSize /= 1024;
+            }
+            return string.Format("{0:n" + decimalPlaces + "} {1}",
+                adjustedSize,
+                Suffixes[mag]);
+        }
+    }
+}
