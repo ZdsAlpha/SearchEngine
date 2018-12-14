ce58c1983f51df4692a8bdf1715bb6da32e6e217
ZdsAlpha <abzsoft999@gmail.com>
2018-12-14 09:30:10 +0500

    Added XML processing

--- Indexer/Helper/Functions.cs
+++ Indexer/Helper/Functions.cs
@@ -7,12 +7,12 @@ namespace Helper
 {
     public static class Functions
     {
-        static readonly string[] Suffixes =
-                   {"", "K", "M", "G", "T", "P", "E", "Z", "Y" };
-        static string Suffix(long value, int decimalPlaces = 2)
+        static readonly string[] SizeSuffixes =
+                   {"bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+        static string SizeSuffix(long value, int decimalPlaces = 2)
         {
             if (decimalPlaces < 0) { throw new ArgumentOutOfRangeException("decimalPlaces"); }
-            if (value < 0) { return "-" + Suffix(-value); }
+            if (value < 0) { return "-" + SizeSuffix(-value); }
             if (value == 0) { return string.Format("{0:n" + decimalPlaces + "} bytes", 0); }
             int mag = (int)Math.Log(value, 1024);
             decimal adjustedSize = (decimal)value / (1L << (mag * 10));
@@ -23,7 +23,7 @@ namespace Helper
             }
             return string.Format("{0:n" + decimalPlaces + "} {1}",
                 adjustedSize,
-                Suffixes[mag]);
+                SizeSuffixes[mag]);
         }
     }
 }
--- Indexer/Repository/Program.cs
+++ Indexer/Repository/Program.cs
@@ -1,14 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml;
+using Helper;
 
 namespace Repository
 {
     class Program
     {
+        const string wikipediaPath = "wikipedia.xml";
+        const string wikitextPath = "wikitext.txt";
+        const string pageTitleList = "titles.txt";
+        const string pageTitleIndex = "titles.index";
+        const string redirects = "redirects.bin";
         static void Main(string[] args)
         {
+            Wikitext();
+        }
+        static void Wikitext()
+        {
+            Directory.CreateDirectory("Wikitext\\");
+            FileStream wikipedia = new FileStream("wikipedia.xml", FileMode.Open, FileAccess.Read, FileShare.None, 1024 * 1024 * 2);
+            using (XmlReader reader = XmlReader.Create(wikipedia))
+            {
+                bool is_page = false;
+                int ns = 0;
+                string title = null;
+                string redirect = null;
+                string content = null;
+
+                string seeking = "";
+
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            switch (reader.Name)
+                            {
+                                case "page":
+                                    is_page = true;
+                                    ns = 0;
+                                    title = null;
+                                    redirect = null;
+                                    content = null;
+                                    seeking = "";
+                                    break;
+                                case "ns":
+                                    seeking = "ns";
+                                    break;
+                                case "title":
+                                    seeking = "title";
+                                    break;
+                                case "redirect":
+                                    redirect = reader.GetAttribute("title");
+                                    break;
+                                case "text":
+                                    seeking = "content";
+                                    break;
+                                default:
+                                    break;
+                            }
+                            break;
+                        case XmlNodeType.Text:
+                            if (!is_page)
+                                continue;
+                            switch (seeking)
+                            {
+                                case "ns":
+                                    ns = int.Parse(reader.Value);
+                                    seeking = "";
+                                    break;
+                                case "title":
+                                    title = reader.Value;
+                                    seeking = "";
+                                    break;
+                                case "content":
+                                    content = reader.Value;
+                                    seeking = "";
+                                    break;
+                                default:
+                                    break;
+                            }
+                            break;
+                        case XmlNodeType.EndElement:
+                            if (reader.Name == "page")
+                            {
+                                if (redirect == null && ns == 0)
+                                    File.WriteAllText("Wikitext\\" + title + ".txt",content,Encoding.UTF8);
+                                is_page = false;
+                                ns = 0;
+                                title = null;
+                                redirect = null;
+                                content = null;
+                                seeking = "";
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
         }
     }
 }
